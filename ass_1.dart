fcb520b727421811b471cd113aead5dc2251f51d
Mudasir41302 <81384345+Mudasir41302@users.noreply.github.com>
2022-02-27 21:41:06 +0500

    Add files via upload

---
+++ ass_1.dart
@@ -0,0 +1,44 @@
+void main(){
+
+    // task no 1
+    // print twinkle twinkle
+
+    print('Twinkle, Twinkle little star\n\tHow I Wonder What you are!\n\t\tup above the world so high\n\t\tlike a daimond in the sky\n\tTwinkle, Twinkle little star\nHow I Wonder What you are');
+
+
+
+    // task no 2
+    //declare variables & print in cmd
+
+    String fName = 'Mudasir';
+    String lName = 'Hussain';
+    var a = '19 years old';
+    var sal = 'rs 0';
+    print('${fName} ${lName} ${a} ${sal}');
+
+
+
+    // task no 3
+    // check variable types
+    
+    String firstName = 'Mudasir';
+    String lastName = 'Hussain';
+    var age = '19 years old';
+    var salary = 'rs 0';
+    print(firstName.runtimeType);
+    print(lastName.runtimeType);
+    print(age.runtimeType);
+    print(salary.runtimeType);
+
+
+    // task no 4
+    //declare 2 variables and perform +-*/%
+    var num_1 = 4;
+    var num_2 = 2;
+    print('plus ${num_1+num_2}');
+    print('minus ${num_1-num_2}');
+    print('multiply ${num_1*num_2}');
+    print('divide ${num_1/num_2}');
+    print('modulus ${num_1%num_2}');
+
+}
