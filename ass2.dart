dfee00ff7bd7226b20240ed1dbd29eb485ffc7c1
MUDASIR HUSSAIN <81384345+Mudasir41302@users.noreply.github.com>
2022-03-15 14:26:15 +0500

    Add files via upload

---
+++ ass2.dart
@@ -0,0 +1,136 @@
+import 'dart:io';
+void main(){
+    // task no 1
+
+    // stdout.write('enter your name:');
+    // var name = stdin.readLineSync();
+    // stdout.write('Which Product do you want to buy?:');
+    // var productTitle = stdin.readLineSync();
+    // stdout.write('How many Products do you want to order Please enter your quantity?:');
+    // var quantity = stdin.readLineSync();
+    // print('${name} ordered ${quantity} ${productTitle} on xyz clothing store');
+
+
+
+    // task no 2
+    // stdout.write('enter first number');
+    // var num1 = int.parse(stdin.readLineSync()!);
+    // stdout.write('enter first number');
+    // var num2 = int.parse(stdin.readLineSync()!);
+    // var add = num1 + num2;
+    // print(add);
+
+    
+    // task no 3
+    // stdout.write('enter first number');
+    // var num1 = int.parse(stdin.readLineSync()!);
+    // stdout.write('enter first number');
+    // var num2 = int.parse(stdin.readLineSync()!);
+    // var sub = num1 - num2;
+    // print(sub);
+
+
+
+    
+    // task no 3
+    // stdout.write('enter first number');
+    // var num1 = int.parse(stdin.readLineSync()!);
+    // stdout.write('enter first number');
+    // var num2 = int.parse(stdin.readLineSync()!);
+    // var div = num1 / num2;
+    // print(div);
+
+
+    
+    // task no 3
+    // stdout.write('enter first number');
+    // var num1 = int.parse(stdin.readLineSync()!);
+    // stdout.write('enter first number');
+    // var num2 = int.parse(stdin.readLineSync()!);
+    // var multi = num1 * num2;
+    // print(multi);
+
+
+    // task no 3
+    // stdout.write('enter first number');
+    // var num1 = int.parse(stdin.readLineSync()!);
+    // stdout.write('enter first number');
+    // var num2 = int.parse(stdin.readLineSync()!);
+    // var modulus = num1 % num2;
+    // print(modulus);
+
+
+    // task 4
+    // var ticketPrice = 600;
+    // print('cost of 1 movie ticket is 600 PKR');
+    // stdout.write('how many tickets do you want to buy? please enter quantity');
+    // var quantity = int.parse(stdin.readLineSync()!);
+    // var total = ticketPrice * quantity;
+    // print('your quantity ${quantity}');
+    // print('total price ${total}');
+
+
+
+    // task 5
+    // stdout.write('enter celcius temperature');
+    // var celcius = int.parse(stdin.readLineSync()!);
+    // var farenheit = (celcius *(9/5))+32;
+    // print('farenheit ${farenheit}');
+
+
+
+
+    // task 6
+    
+    // var item1 = 600;
+    // var item2 = 300;
+    // print('price of 1 item is 600');
+    // stdout.write('quantity of 1 item is ');
+    // var quantityItem1 = int.parse(stdin.readLineSync()!);
+
+    // print('price of 2 item is 300');
+    // stdout.write('quantity of 2 item is ');
+    // var quantityItem2 = int.parse(stdin.readLineSync()!);
+
+    // print('\nSHOPPING CART');
+    // print('price of item 1 is = ${item1}');
+    // print('quantity of item 1 is = ${quantityItem1}');
+    // print('price of item 1 is = ${item2}');
+    // print('quantity of item 2 is = ${quantityItem2}');
+    // var total = (item1 * quantityItem1) + (item2  * quantityItem2);
+    // print('\nTotal Cost = ${total}');
+
+
+
+
+
+    // task 7
+    // stdout.write('total marks');
+    // var totalMarks = int.parse(stdin.readLineSync()!);
+    // stdout.write('obtained marks');
+    // var obtainedMarks = int.parse(stdin.readLineSync()!);
+    // var percentage = (obtainedMarks / totalMarks)*100;
+    // print('the percentage is ${percentage}');
+
+
+
+
+    // task 8
+    // var usd = 10;
+    // var riyal = 25;
+    // var convartToPkrTotal = (usd*175.70)+(riyal*46.85);
+    // print(convartToPkrTotal);
+
+
+
+    // task 9
+    // stdout.write('enter hours ');
+    // var hours = int.parse(stdin.readLineSync()!);
+    // stdout.write('enter rate per hour  in pkr');
+    // var rph = int.parse(stdin.readLineSync()!);
+    // print('hours ${hours}');
+    // print('hours ${rph}');
+    // var totalSalary = rph*hours ;
+    // print('total salary of one day is ${totalSalary}');
+
+}
