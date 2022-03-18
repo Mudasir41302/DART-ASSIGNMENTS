2d382c55b7352864e1c5fcf02cf9d056cb8cefcf
MUDASIR HUSSAIN <81384345+Mudasir41302@users.noreply.github.com>
2022-03-18 15:43:41 +0500

    Add files via upload

---
+++ ass3.dart
@@ -0,0 +1,99 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() {
+  // task no 1
+
+  // stdout.write('enter first subject name');
+  // var fSub = stdin.readLineSync()!;
+  // stdout.write('enter second subject name');
+  // var sSub = stdin.readLineSync()!;
+  // stdout.write('enter third subject name');
+  // var tSub = stdin.readLineSync()!;
+
+  // var totalMarksForEachSub = 100;
+  // stdout.write('enter first subject number');
+  // var obtFor1stSubject = int.parse(stdin.readLineSync()!);
+  // stdout.write('enter second subject number');
+  // var obtFor2ndSubject = int.parse(stdin.readLineSync()!);
+  // stdout.write('enter third subject number');
+  // var obtFor3rdSubject = int.parse(stdin.readLineSync()!);
+  // print('obtained total marks ${obtFor1stSubject+obtFor2ndSubject+obtFor3rdSubject}');
+  // print('percentage => ${((obtFor1stSubject+obtFor2ndSubject+obtFor3rdSubject)/(totalMarksForEachSub*3))*100} percent');
+
+  // task no 2
+
+  // stdout.write('enter city name');
+  // var cityName = stdin.readLineSync()!;
+  // cityName.toLowerCase();
+  // (cityName == 'karachi')
+  //     ? print('welcome to city of lights')
+  //     : print('please enter karachi');
+
+  // task no 3
+
+  // stdout.write('enter your gender');
+  // var gender = stdin.readLineSync()!;
+  // gender.toLowerCase();
+  // if (gender == 'm' || gender == 'male') {
+  //   print('good morning sir');
+  // } else if (gender == 'f' || gender == 'female') {
+  //   print("good morning ma'am");
+  // } else {
+  //   print('please enter correct gender');
+  // }
+
+  // task no 4
+
+  // stdout.write('enter your car fuel range benteen 0 to 5 ');
+  // var fuelRange = double.parse(stdin.readLineSync()!);
+  // (fuelRange <= 0.25) ? print('please refill the tank') : print('good to go');
+
+  // task no 5
+
+  // stdout.write('enter your area temperature ');
+  // var temp = int.parse(stdin.readLineSync()!);
+  // if (temp > 40) {
+  //   print('it is too hot outside');
+  // } else if (temp > 30) {
+  //   print('todays weather is normal');
+  // } else if (temp > 20) {
+  //   print('todays weather is cool');
+  // } else if (temp > 10) {
+  //   print('OMG! todays weather is so cool');
+  // } else if (temp < 9) {
+  //   print('OMG! todays weather is sooo cool');
+  // } else {
+  //   print('please enter correct values');
+  // }
+
+  // task no 6
+
+  // stdout.write('enter single number or character');
+  // var asci = stdin.readByteSync();
+  // if (asci >= 97 && asci <= 122) {
+  //   print('lower case and the ascii code is ${asci}');
+  // } else if (asci >= 65 && asci <= 90) {
+  //   print('upper case and the ascii code is ${asci}');
+  // } else if (asci >= 48 && asci <= 57) {
+  //   print('number and the ascii code is ${asci}');
+  // } else {
+  //   print('enter only number and alphabet characters');
+  // }
+
+  // task no 7
+
+  // stdout.write('enter first integer');
+  // var first = int.parse(stdin.readLineSync()!);
+  // stdout.write('enter second integer');
+  // var second = int.parse(stdin.readLineSync()!);
+  // if (first > second) {
+  //   print('${first}is greater');
+  // } else if (first < second) {
+  //   print('${second}is greater');
+  // } else if (first == second) {
+  //   print('both are equal');
+  // } else {
+  //   print('please enter correct values');
+  // }
+}
